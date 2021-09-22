01fafe1e79c5e657442c38ea3a88fbec07b7f1d9
Ksenia Pavlenko <mkd2005@rambler.ru>
2021-09-23 00:57:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketPrice = 12_555;
+        int oneBonusMileCost = 20;
+        int bonusMilesCount = ticketPrice / oneBonusMileCost;
+
+        System.out.println(bonusMilesCount);
+    }
+}
